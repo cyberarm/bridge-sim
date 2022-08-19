834a89431d43caf07c5c80cfcfe51668773acb1b
Cyberarm <matthewlikesrobots@gmail.com>
2022-08-18 22:10:35 -0500

    Work on placing segments

--- lib/states/game.rb
+++ lib/states/game.rb
@@ -22,7 +22,10 @@ class BridgeSim
 
               button get_image("#{MEDIA_PATH}/icons/plus.png"), image_height: 1.0
               button get_image("#{MEDIA_PATH}/icons/minus.png"), image_height: 1.0
-              button get_image("#{MEDIA_PATH}/icons/trash.png"), image_height: 1.0
+
+              button(get_image("#{MEDIA_PATH}/icons/trash.png"), image_height: 1.0) do
+                @segments.clear
+              end
             end
 
             # Ready / Reset Button
@@ -46,16 +49,20 @@ class BridgeSim
               para "Vehicles:"
             end
             stack(fill: true, height: 1.0) do
-              para "909090"
+              @score_label = para "909090"
               para ""
-              para "999"
-              para "0/6"
-              para "0/1"
+              @time_label = para "999"
+              @segment_label = para "0/6"
+              @vehicle_label = para "0/1"
             end
           end
         end
 
         @segments = []
+        @anchors = [
+          CyberarmEngine::Vector.new(100, window.height / 2),
+          CyberarmEngine::Vector.new(window.width / 2, window.height / 2),
+        ]
       end
 
       def draw
@@ -65,21 +72,40 @@ class BridgeSim
           Gosu.draw_circle(window.width - 128, 128, 64, 36, 0x77_ffa348, 3000)
 
           if @drag_start
-            Gosu.draw_arc(@drag_start.x, @drag_start.y, 8, 1.0, 36, 2, Gosu::Color::YELLOW, Float::INFINITY)
-            Gosu.draw_line(@drag_start.x, @drag_start.y, Gosu::Color::YELLOW, window.mouse_x, window.mouse_y, Gosu::Color::YELLOW, Float::INFINITY)
+            anchor = nearest_anchor
+            pos = anchor || CyberarmEngine::Vector.new(window.mouse_x, window.mouse_y)
 
-            midpoint = (CyberarmEngine::Vector.new)#(window.mouse_x, window.mouse_y) - @drag_start).normalized
-            midpoint.x = ((@drag_start.x + window.mouse_x) / 2.0)
-            midpoint.y = ((@drag_start.y + window.mouse_y) / 2.0)
+            Gosu.draw_arc(@drag_start.x, @drag_start.y, 8, 1.0, 36, 2, Gosu::Color::YELLOW, Float::INFINITY)
+            Gosu.draw_line(@drag_start.x, @drag_start.y, Gosu::Color::YELLOW, pos.x, pos.y, Gosu::Color::YELLOW, Float::INFINITY)
+
+            Gosu.draw_arc(pos.x, pos.y, 8, 1.0, 36, 2, Gosu::Color::YELLOW, Float::INFINITY) if anchor
+          end
+
+          @anchors.each do |vector|
+            next unless nearest_anchor == vector
+            next if anchor_in_use(vector)
+
+            Gosu.draw_arc(vector.x, vector.y, 8, 1.0, 36, 2, Gosu::Color::YELLOW, Float::INFINITY)
+          end
+
+          @segments.each do |segment|
+            point_a = segment.first
+            point_b = segment.last
+            midpoint = CyberarmEngine::Vector.new((point_a.x + point_b.x) / 2.0, (point_a.y + point_b.y) / 2.0)
+
+            Gosu.draw_arc(point_a.x, point_a.y, 8, 1.0, 36, 2, Gosu::Color::YELLOW, Float::INFINITY)
+            Gosu.draw_arc(point_b.x, point_b.y, 8, 1.0, 36, 2, Gosu::Color::YELLOW, Float::INFINITY)
+            Gosu.draw_line(point_a.x, point_a.y, Gosu::Color::YELLOW, point_b.x, point_b.y, Gosu::Color::YELLOW, Float::INFINITY)
 
             Gosu.draw_arc(midpoint.x, midpoint.y, 8, 1.0, 36, 2, Gosu::Color::CYAN, Float::INFINITY)
-            Gosu.draw_arc(window.mouse_x, window.mouse_y, 8, 1.0, 36, 2, Gosu::Color::YELLOW, Float::INFINITY)
           end
         end
       end
 
       def update
         super
+
+        @segment_label.value = "#{@segments.size}/?"
       end
 
       def button_down(id)
@@ -87,7 +113,9 @@ class BridgeSim
 
         case id
         when Gosu::MS_LEFT
-          @drag_start = CyberarmEngine::Vector.new(window.mouse_x, window.mouse_y)
+          if (anchor = nearest_anchor)
+            @drag_start = anchor unless anchor_in_use(anchor)
+          end
         end
       end
 
@@ -96,8 +124,24 @@ class BridgeSim
 
         case id
         when Gosu::MS_LEFT
-          (CyberarmEngine::Vector.new(window.mouse_x, window.mouse_y) - @drag_start)
+          if @drag_start && (anchor = nearest_anchor)
+            @segments << [@drag_start, anchor] if anchor != @drag_start
+          end
+
           @drag_start = nil
+        end
+      end
+
+      def nearest_anchor(pos = CyberarmEngine::Vector.new(window.mouse_x, window.mouse_y))
+        @anchors.find { |v| v.distance(pos) <= 16 }
+      end
+
+      def anchor_in_use(anchor)
+        @segments.find do |segment|
+          point_a = segment.first
+          point_b = segment.last
+
+          point_a == anchor || point_b == anchor
         end
       end
     end
